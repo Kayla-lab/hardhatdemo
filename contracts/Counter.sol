2a990a4717263cc58ecae69a603f99b3e8c6852b
Kayla <uk3y9l1xy3g9@hotmail.com>
2025-02-17 20:23:29 +0800

    map

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.19;
 
 contract Counter {
     uint256 private count;
+    mapping(address => uint256) private userCounts;
+    address[] private users;
     
-    event Incremented(uint256 newCount);
-    event Decremented(uint256 newCount);
+    event Incremented(uint256 newCount, address user);
+    event Decremented(uint256 newCount, address user);
+    event UserRecorded(address user, uint256 userCount);
     
     constructor() {
         count = 0;
@@ -13,13 +16,15 @@ contract Counter {
     
     function increment() public {
         count += 1;
-        emit Incremented(count);
+        _recordUser();
+        emit Incremented(count, msg.sender);
     }
     
     function decrement() public {
         require(count > 0, "Count cannot be negative");
         count -= 1;
-        emit Decremented(count);
+        _recordUser();
+        emit Decremented(count, msg.sender);
     }
     
     function getCount() public view returns (uint256) {
@@ -28,5 +33,25 @@ contract Counter {
     
     function reset() public {
         count = 0;
+    }
+    
+    function _recordUser() private {
+        if (userCounts[msg.sender] == 0) {
+            users.push(msg.sender);
+        }
+        userCounts[msg.sender] += 1;
+        emit UserRecorded(msg.sender, userCounts[msg.sender]);
+    }
+    
+    function getUserCount(address user) public view returns (uint256) {
+        return userCounts[user];
+    }
+    
+    function getAllUsers() public view returns (address[] memory) {
+        return users;
+    }
+    
+    function getTotalUsers() public view returns (uint256) {
+        return users.length;
     }
 }
